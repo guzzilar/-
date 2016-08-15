8243866483e7f73f6cfe422310adfad99fac25c9
guzzilar <iam.nuttanon@gmail.com>
2016-08-16 01:07:43 +0700

    Add adjectives-01 lesson (preparing)

---
+++ lessons/adjectives-01.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>TH Lessons - Adjectives 01</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <style type="text/css">
+      audio {
+        width: 40px;
+        display: block;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <table class="table table-striped table-bordered">
+            <thead>
+              <tr style="background: #FFFF99">
+                <th>English</th>
+                <th>Japanese</th>
+                <th>Kana</th>
+                <th>Thai</th>
+                <th>(TH Kana)</th>
+                <th>Listen</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr> 
+                <td>Big</td>
+                <td>大きい</td>
+                <td>おおきい</td>
+                <td>ใหญ่</td>
+                <td>ヤイ (rising tone)</td>
+                <td>
+                  <audio controls width="10px">
+                    <source  src="resources/adjectives-01/yai.mp3" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                  </audio>
+                </td>
+              </tr>
+              <tr> 
+                <td>Small</td>
+                <td>小さい</td>
+                <td>ちいさい</td>
+                <td>เล็ก</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Cheap</td>
+                <td>安い</td>
+                <td>やすい</td>
+                <td>ถูก</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Expensive</td>
+                <td>高い</td>
+                <td>たかい</td>
+                <td>แพง</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Dark</td>
+                <td>暗い</td>
+                <td>くらい</td>
+                <td>มืด</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Light</td>
+                <td>明るい</td>
+                <td>あかるい</td>
+                <td>สว่าง</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Delicious</td>
+                <td>-</td>
+                <td>おいしい</td>
+                <td>อร่อย</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Bad tasting</td>
+                <td>-</td>
+                <td>まずい</td>
+                <td>ไม่อร่อย</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Difficult</td>
+                <td>難しい</td>
+                <td>むずかしい</td>
+                <td>ยาก</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Good</td>
+                <td>いい / 良い</td>
+                <td>いい / よい</td>
+                <td>ดี</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Bad</td>
+                <td>悪い</td>
+                <td>わるい</td>
+                <td>แย่</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Heavy</td>
+                <td>重い</td>
+                <td>おもい</td>
+                <td>หนัก</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Light (in weight)</td>
+                <td>軽い</td>
+                <td>かるい</td>
+                <td>เบา</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Hot</td>
+                <td>熱い</td>
+                <td>あつい</td>
+                <td>ร้อน</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Hot (weather) </td>
+                <td>暑い</td>
+                <td>あつい</td>
+                <td>ร้อน</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+               <tr> 
+                <td>Warm</td>
+                <td>温かい</td>
+                <td>あたたかい</td>
+                <td>อุ่น</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Warm (weather)</td>
+                <td>暖かい</td>
+                <td>あたたかい</td>
+                <td>อุ่น</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Cold</td>
+                <td>冷たい</td>
+                <td>つめたい</td>
+                <td>เย็น</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Cold (weather)</td>
+                <td>寒い</td>
+                <td>さむい</td>
+                <td>เย็น</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Hot and humid</td>
+                <td>蒸し暑い</td>
+                <td>むしあつい</td>
+                <td>ร้อนชื้น</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Long</td>
+                <td>長い</td>
+                <td>ながい</td>
+                <td>ยาว</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Short (in length)</td>
+                <td>短い</td>
+                <td>みじかい</td>
+                <td>สั้น</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>New</td>
+                <td>新しい</td>
+                <td>あたらしい</td>
+                <td>ใหม่</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Old</td>
+                <td>古い</td>
+                <td>ふるい</td>
+                <td>เก่า</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Narrow</td>
+                <td>狭い</td>
+                <td>せまい</td>
+                <td>แคบ</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Wide</td>
+                <td>広い</td>
+                <td>ひろい</td>
+                <td>กว้าง</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Tall</td>
+                <td>高い</td>
+                <td>たかい</td>
+                <td>สูง</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Short (in height)</td>
+                <td>低い</td>
+                <td>ひくい</td>
+                <td>สั้น (general) / เตี้ย (a bit casual)</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Quick</td>
+                <td>速い</td>
+                <td>はやい</td>
+                <td>เร็ว</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Slow</td>
+                <td>遅い</td>
+                <td>おそい</td>
+                <td>ช้า</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Interesting</td>
+                <td>面白い</td>
+                <td>おもしろい</td>
+                <td>น่าสนใจ</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Busy</td>
+                <td>忙しい</td>
+                <td>いそがしい</td>
+                <td>ยุ่ง</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Fun</td>
+                <td>楽しい</td>
+                <td>たのしい</td>
+                <td>สนุก</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr> 
+                <td>Cool/attractive/has good form</td>
+                <td>格好いい</td>
+                <td>かっこういい</td>
+                <td>เท่</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>Early</td>
+                <td>早い</td>
+                <td>はやい</td>
+                <td>เร็ว</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>Late</td>
+                <td>晩い</td>
+                <td>おそい</td>
+                <td>ช้า / สาย</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
